44c9bc436c3b042e97b6676a807d35ca4331cdfe
annwon603 <aniwong603@gmail.com>
2024-02-19 18:49:21 -0800

    Dirt Block are Walkable
    1. Made a Script to assign MeshCollider to every dirt block in the array

---
+++ Assets/Scripts/SetDirtCollider.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetDirtCollider : MonoBehaviour
+{
+    public Mesh mesh;
+    public GameObject[] dirt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Sets up meshcollider in each dirt block
+        foreach(GameObject block in dirt){
+            MeshCollider mc = block.AddComponent<MeshCollider>();
+            mc.sharedMesh = mesh;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
