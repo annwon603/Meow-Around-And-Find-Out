9b16692e25d8a42f701d8899a7d845027264544d
annwon603 <aniwong603@gmail.com>
2024-03-09 16:35:02 -0800

    Finished programming Dialouge UI
    I literally need to drink to cope myself from debugging

---
+++ Assets/Scripts/NPCDetection.cs
@@ -0,0 +1,53 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+using UnityEngine.UI;
+
+public class NPCDetection : UdonSharpBehaviour
+{
+
+    public bool isPlayerThere = false;
+
+    public UdonBehaviour NPCInteraction;
+
+    public NPCInteract npcScript;
+
+    private GameObject dialouge;
+    private GameObject theChoseOne;
+    int enter = 0;
+    int exit = 0;
+
+    public void Start()
+    {
+        dialouge = (GameObject)NPCInteraction.GetProgramVariable("dialouge");
+        theChoseOne = (GameObject)NPCInteraction.GetProgramVariable("theChoseOne");
+    }
+
+    public void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        isPlayerThere = true;
+        enter++;
+        Debug.Log("Player Enter " + enter + " "+ isPlayerThere);
+
+    }
+
+    public void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        isPlayerThere = false;
+        dialouge.SetActive(false);
+        theChoseOne.SetActive(false);
+        exit++;
+        Debug.Log("Player Exit" + exit+ " "+ isPlayerThere);
+    }
+    void Update()
+    {
+
+        dialouge = (GameObject)NPCInteraction.GetProgramVariable("dialouge");
+        theChoseOne = (GameObject)NPCInteraction.GetProgramVariable("theChoseOne");
+    }
+
+   
+}
---
+++ Assets/Scripts/NPCInteract.cs
@@ -0,0 +1,67 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+using UnityEngine.UI;
+
+public class NPCInteract : UdonSharpBehaviour
+{
+    //Accessing to Detection script
+    public UdonBehaviour theOtherScript;
+    public NPCDetection script; 
+
+    //Accessing to chatbox
+    public GameObject dialouge;
+
+    //Accessing to index to list of dialouge
+    public int Type; 
+
+    public GameObject[] listOfDialouges;
+
+    //The chosen dialouge to put into chat box
+    GameObject theChoseOne;
+    private bool canClick;
+
+    bool hasClick = false;
+    void Start()
+    {
+        dialouge.SetActive(false);
+        theChoseOne = listOfDialouges[Type];
+
+        theChoseOne.SetActive(false);
+    }
+
+    void Update()
+    {
+        canClick = (bool)theOtherScript.GetProgramVariable("isPlayerThere");
+        theChoseOne = listOfDialouges[Type];
+
+
+    }
+
+    public override void Interact()
+    {
+        Debug.Log(canClick);
+        if(hasClick == false && canClick == true){
+            Debug.Log("You're talking to NPC");
+            dialouge.SetActive(true);
+            theChoseOne.SetActive(true);
+            hasClick = true;
+        }else{
+            dialouge.SetActive(false);
+            theChoseOne.SetActive(false);
+            hasClick = false;
+        }
+
+    }
+
+    // public void SetOff()
+    // {
+    //     dialouge.SetActive(false);
+    //     theChoseOne.SetActive(false);
+    //     hasClick = false;
+
+    // }
+}
